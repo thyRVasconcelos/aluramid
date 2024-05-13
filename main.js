e97ff493bb9ac5cf39d6fa370c9d8877cea607c6
thiago <202307027278@alunos.unifanor.edu.br>
2024-05-13 11:21:53 -0300

    1 comit

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom(seletorElemento) {
+  const elemento = document.querySelector(seletorElemento);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log("Elemento não encontrado ou não é um áudio");
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  //template String
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
